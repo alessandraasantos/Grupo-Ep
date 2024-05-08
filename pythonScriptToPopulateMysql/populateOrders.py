731e3e0eb15b7f30bb846ccdd9ad173e8aeecfc4
Leticia Paoleschi <100284453+leticiapaoleschi@users.noreply.github.com>
2024-05-07 23:15:52 -0300

    Update populateOrders.py

--- pythonScriptToPopulateMysql/populateOrders.py
+++ pythonScriptToPopulateMysql/populateOrders.py
@@ -2,7 +2,7 @@ import csv
 import mysql.connector
 from datetime import datetime
 
-# Connect to MySQL database
+
 cnx = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -11,27 +11,26 @@ cnx = mysql.connector.connect(
 )
 cursor = cnx.cursor()
 
-# Open Orders CSV file
 with open(r'C:\Users\marcu\Downloads\archive\List of Orders.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
 
-    # Skip header row
+
     next(reader)
 
-    # Iterate over rows and insert into Orders table
+
     for row in reader:
-        # Check if the row is empty (contains only empty strings)
+        
         if not any(row):
-            continue  # Skip empty row
+            continue  
         
         order_id = row[0]
         order_date_str = row[1]
         
-        # Check if the order ID is empty
+
         if not order_id:
-            continue  # Skip row if order ID is empty
+            continue  
         
-        # Parse and format the date
+
         order_date = datetime.strptime(order_date_str, '%d-%m-%Y').date()
         
         customer_name = row[2]
@@ -40,7 +39,7 @@ with open(r'C:\Users\marcu\Downloads\archive\List of Orders.csv', newline='') as
         query = "INSERT INTO Orders (order_id, order_date, customer_name, state, city) VALUES (%s, %s, %s, %s, %s)"
         cursor.execute(query, (order_id, order_date, customer_name, state, city))
 
-# Commit changes and close connection
+
 cnx.commit()
 cursor.close()
 cnx.close()
