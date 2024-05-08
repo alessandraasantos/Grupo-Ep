f67897b4992c908e2afc305e5dabfd56062d43ce
Leticia Paoleschi <100284453+leticiapaoleschi@users.noreply.github.com>
2024-05-07 23:17:47 -0300

    Update MySqlTargetSales_to_MongoDB.py

--- PythonScriptsToMigrateToMongoDB/MySqlTargetSales_to_MongoDB.py
+++ PythonScriptsToMigrateToMongoDB/MySqlTargetSales_to_MongoDB.py
@@ -1,34 +1,32 @@
 from pymongo import MongoClient
 import mysql.connector
 
-# Connect to MongoDB
-mongo_client = MongoClient('mongodb://localhost:27017/')
-mongo_db = mongo_client['db_ecommerce']  # Replace 'your_mongodb_database' with your MongoDB database name
-mongo_collection = mongo_db['target_sales']  # Replace 'your_mongodb_collection' with your MongoDB collection name
 
-# Connect to MySQL
+mongo_client = MongoClient('mongodb://localhost:27017/')
+mongo_db = mongo_client['db_ecommerce']  
+mongo_collection = mongo_db['target_sales']  
+
+
 mysql_connection = mysql.connector.connect(
     host='localhost',
     user='root',
     password='root',
     database='db_ecommerce'
-)  # Replace the placeholders with your MySQL connection details
+)  
 
-# Fetch data from MySQL tables
+
 cursor = mysql_connection.cursor(dictionary=True)
 
-# Example: Fetch data from MySQL table 'users'
+
 cursor.execute("SELECT * FROM target_sales")
 mysql_targetSaless_data = cursor.fetchall()
 
-# Transform MySQL data into MongoDB documents (this step may involve restructuring the data)
 
-# Example: Transform 'users' data
 mongo_targetSales_data = []
 for targetSales in mysql_targetSaless_data:
     order_date_iso = targetSales['order_date'].isoformat()
     mongo_targetSales = {
-        '_id': targetSales['id_target_sales'],  # Assuming 'id' is the primary key
+        '_id': targetSales['id_target_sales'], 
         'month_of_order_date': targetSales['month_of_order_date'],
         'category': targetSales['category'],
         'target': targetSales['target'],
@@ -37,10 +35,9 @@ for targetSales in mysql_targetSaless_data:
     }
     mongo_targetSales_data.append(mongo_targetSales)
 
-# Insert data into MongoDB
+
 mongo_collection.insert_many(mongo_targetSales_data)
 
 
-# Close connections
 mongo_client.close()
 mysql_connection.close()
