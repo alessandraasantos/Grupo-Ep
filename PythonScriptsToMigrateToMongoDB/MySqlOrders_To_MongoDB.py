fe3abdb0711bda84901b501208eb2153b1347e62
Leticia Paoleschi <100284453+leticiapaoleschi@users.noreply.github.com>
2024-05-07 23:18:54 -0300

    Update MySqlOrders_To_MongoDB.py

--- PythonScriptsToMigrateToMongoDB/MySqlOrders_To_MongoDB.py
+++ PythonScriptsToMigrateToMongoDB/MySqlOrders_To_MongoDB.py
@@ -3,36 +3,35 @@ import mysql.connector
 from decimal import Decimal
 from datetime import datetime
 
-# Connect to MongoDB
-mongo_client = MongoClient('mongodb://localhost:27017/')
-mongo_db = mongo_client['db_ecommerce']  # Replace 'your_database' with your MongoDB database name
-order_details_collection = mongo_db['OrderDetails']  # Collection for OrderDetails
 
-# Connect to MySQL
+mongo_client = MongoClient('mongodb://localhost:27017/')
+mongo_db = mongo_client['db_ecommerce']  
+order_details_collection = mongo_db['OrderDetails']  
+
+
 mysql_connection = mysql.connector.connect(
     host='localhost',
     user='root',
     password='root',
     database='db_ecommerce'
-)  # Replace the placeholders with your MySQL connection details
+)  
 
-# Fetch data from MySQL table 'OrderDetails'
+
 cursor = mysql_connection.cursor(dictionary=True)
 cursor.execute("SELECT od.*, o.order_date, o.customer_name, o.state, o.city FROM OrderDetails od JOIN Orders o ON od.order_id = o.order_id")
 mysql_order_details_data = cursor.fetchall()
 
-# Migrate OrderDetails
 order_details_data = []
 for order_detail in mysql_order_details_data:
-    # Convert Decimal objects to float
+  
     amount = float(order_detail['amount'])
     profit = float(order_detail['profit'])
 
-    # Convert order_date to datetime object
+
     order_date = datetime.combine(order_detail['order_date'], datetime.min.time())
 
     order_detail_data = {
-        '_id': order_detail['order_detail_id'],  # Assuming 'order_detail_id' is unique
+        '_id': order_detail['order_detail_id'],  
         'order_id': order_detail['order_id'],
         'amount': amount,
         'profit': profit,
@@ -43,13 +42,13 @@ for order_detail in mysql_order_details_data:
         'customer_name': order_detail['customer_name'],
         'state': order_detail['state'],
         'city': order_detail['city']
-        # Add more fields as needed
+        
     }
     order_details_data.append(order_detail_data)
 
-# Insert OrderDetails data into MongoDB
+
 order_details_collection.insert_many(order_details_data)
 
-# Close connections
+
 mysql_connection.close()
 mongo_client.close()
